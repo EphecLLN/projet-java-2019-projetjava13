f0fb00d498cd6e95497ea7da8dabde47eb9ebbb4
Gaelol <37298427+Gaelol@users.noreply.github.com>
2019-12-17 03:01:08 +0100

    Update ProjetController.java

--- controller/ProjetController.java
+++ controller/ProjetController.java
@@ -1,33 +1,65 @@
 package projet.controller;
 
-import projet.model.*;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import projet.model.Bonus;
+import projet.model.Cliqueur;
+import projet.view.ProjetGUI;
 import projet.view.ProjetVue;
 
 public class ProjetController {
-	Cliqueur model;
+	Bonus model;
 	ProjetVue vue;
 	
-	public ProjetController(Cliqueur model) {
+	public ProjetController(Bonus model) {
 		this.model = model;
 	}
 	
 	public void incremente() {
 		model.add();
-		
+		model.text();
+		vue.actualise();
 	}
 	
 	public void bonusUn() {
 		model.bonusUn();
+		model.text();
+		vue.actualise();
 	}
 	
 	public void bonusDeux() {
 		model.bonusDeux();
+		model.text();
+		vue.actualise();
 	}
 	public void bonusTrois() {
 		model.bonusTrois();
+		model.text();
+		vue.actualise();
+	}
+	public void bonusQuatre() {
+		model.bonusQuatre();
+		model.text();
+		vue.actualise();
+	}
+	public void enregistre() {
+		model.enregistre();
+		model.text();
+		vue.actualise();
+	}
+	
+	public void resultats() {
+		model.Collection();
+		model.text();
+		vue.actualise();
 	}
 	
 	public void addView(ProjetVue vue) {
 		this.vue = vue;
 	}
+	
+	
 }
